8909403e4187c00c93f49ca43605a23af61559f1
chethanck <chethanckm66@gmail.com>
2022-01-09 13:33:34 +0530

    query changes

--- index.js
+++ index.js
@@ -7,28 +7,37 @@ app.use(cors());
 
 const posts = {};
 
-app.get("/posts", (req, res) => {
-    res.send(posts);
+app.get('/posts', (req, res) => {
+  res.send(posts);
 });
 
-app.post("/events", (req, res) => {
-    const { type, data } = req.body;
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
 
-    if (type === "PostCreated") {
-        const { id, title } = data;
-        posts[id] = { id, title, comments: [] };
-    }
-    if (type === "CommentCreated") {
-        const { id, content, postId } = data;
+  if (type === 'PostCreated') {
+    const { id, title } = data;
+    posts[id] = { id, title, comments: [] };
+  }
+  if (type === 'CommentCreated') {
+    const { id, content, postId, status } = data;
 
-        const post = posts[postId];
-        post.comments.push({ id, content });
-    }
+    const post = posts[postId];
+    post.comments.push({ id, content, status });
+  }
 
-    console.log(posts);
-    res.send({});
+  if (type === 'CommentUpdated') {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    const comment = post.comments.find((comment) => comment.id === id);
+    comment.content = content;
+    comment.status = status;
+  }
+
+  console.log(posts);
+  res.send({});
 });
 
 app.listen(4002, () => {
-    console.log("Listen to query server 4002");
+  console.log('Listen to query server 4002');
 });
